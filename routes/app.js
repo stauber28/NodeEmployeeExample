974ddbc82aa409fa4f3be2b8c6a1224c4ba0ea48
Garrett Stauber <stauber28@gmail.com>
2020-02-20 18:25:00 -0500

    Clean up app routes

--- routes/app.js
+++ routes/app.js
@@ -4,20 +4,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const { validationResult, body } = require('express-validator');
-const fetch = require("node-fetch");
-const employeeRoutes = require('./employee');
-
+// Return index view at url root
 router.get('', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'));
-});
-
-router.post('', (req, res) => {
-  req.post({url: '/api/employees', headers: req.headers});
-
-  processRequest(req);
-  res.setHeader('Content-Type', 'application/json');
-  res.send('Req OK');
 });
 
 module.exports = router;
